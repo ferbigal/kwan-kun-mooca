fecb74eb5123c4fa4896e66a38cffa625a2536a1
Fernando Bigal <big_frz@hotmail.com>
2015-06-10 03:30:48 -0300

    Minor Update
    Texts and stylesheet changes
    Inclusion of schedule table

---
+++ Scripts/Index.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    if ($('#modelValid').val() == "modelInvalid") {
+        $('html, body').animate({ scrollTop: $('#contato').position().top }, 'fast');
+        $('#modelValid').val("modelValid");
+        bootbox.alert("Por favor verifique os dados do contato.");
+    }
+
+    $('.carousel').carousel({
+        interval: 5000
+    });
+
+    $("#inputTelefone").mask("(00) 0000-00009");
+});
